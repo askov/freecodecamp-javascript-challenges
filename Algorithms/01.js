3da02e44430850669528e73841a3031e0fea18fc
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-08-27 23:00:05 +0700

    basic bubble sort

---
+++ Algorithms/01.js
@@ -0,0 +1,17 @@
+/**
+* The most basic version of bubble sort O(n2). Modifies arg
+* @param {Array} array Array to sort
+* @returns {Array} Sorted array
+*/
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = array.length - 1; j > i; j--) {
+      if (array[j - 1] > array[j]) {
+        let x = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = x;
+      }
+    }
+  }
+  return array;
+}
