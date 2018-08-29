e038b0a743d3b02bfcb34e9c314ebeb3ea0e6191
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-08-29 22:20:02 +0700

    insertion search

--- Algorithms/01.js
+++ Algorithms/01.js
@@ -37,3 +37,22 @@ function selectionSort(array) {
   }
   return array;
 }
+
+/**
+* Insertion sort O(n2).
+* @param {Array} array Array to sort
+* @returns {Array} Sorted array
+*/
+function insertionSort(array) {
+
+  for (let i = 1; i < array.length; i++) {
+    const key = array[i];
+    let j = i - 1;
+    while (j > 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
+  return array;
+}
