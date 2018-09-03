9b6011db819d48061a4bbab4a8ad6ba8f545ea08
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-09-03 22:45:11 +0700

    priority queue

---
+++ DataStructures/PriorityQueue.js
@@ -0,0 +1,42 @@
+function PriorityQueue() {
+  this.collection = [];
+
+  this.printCollection = function () {
+    console.log(this.collection);
+  };
+
+  this.enqueue = function (el) {
+    let pos = this.collection.length;
+    for (let i = 0; i < this.collection.length; i++) {
+      if (el[1] < this.collection[i][1]) {
+        pos = i;
+        break;
+      }
+    }
+    this.collection.splice(pos, 0, el);
+  };
+
+  this.dequeue = function () {
+    return this.collection.shift()[0];
+  };
+
+  this.isEmpty = function () {
+    return this.collection.length === 0;
+  };
+
+  this.size = function () {
+    return this.collection.length;
+  };
+
+  this.front = function () {
+    return this.collection[0][0];
+  };
+}
+
+
+const pq = new PriorityQueue();
+
+pq.enqueue(['dodo', 1]);
+pq.enqueue(['elephant', 3]);
+pq.enqueue(['human', 1]);
+console.log(pq.collection);
