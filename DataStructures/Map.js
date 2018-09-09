4c0b2a8c9e3c4fcf6c2543fc8edcdf85b12dc5e6
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-09-09 22:28:40 +0700

    Map class

---
+++ DataStructures/Map.js
@@ -0,0 +1,32 @@
+var Map = function () {
+  this.collection = {};
+
+  this.add = function (key, value) {
+    this.collection[key] = value;
+  }
+  this.remove = function (key, value) {
+    delete this.collection[key];
+  }
+  this.get = function (key) {
+    return this.collection[key];
+  }
+  this.has = function (key) {
+    return this.collection.hasOwnProperty(key);
+  }
+  this.values = function (key) {
+    return Object.values(this.collection)
+  }
+  this.size = function () {
+    return Object.keys(this.collection).length;
+  }
+  this.clear = function () {
+    this.collection = {};
+  }
+}
+
+const m = new Map();
+
+m.add('foo', 'bar');
+// m.has('bar');
+console.log(m.has('bar'));
+console.log(m.has('baz'));
