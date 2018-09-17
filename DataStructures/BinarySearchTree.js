ed7a637f04e35a6bedefeaef107b614de5c8b833
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-09-17 21:59:46 +0700

    BinarySearchTree class: add

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -25,7 +25,46 @@ function BinarySearchTree() {
     }
     return el.value;
   }
+  this.add = function(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+      return;
+    }
+    let el = this.root;
+
+    while (true) {
+      if (value < el.value) {
+        if (el.left) {
+          el = el.left;
+        } else {
+          el.left = new Node(value);
+          return;
+        }
+      } else if (value > el.value) {
+        if (el.right) {
+          el = el.right;
+        } else {
+          el.right = new Node(value);
+          return;
+        }
+      } else {
+        return null;
+      }
+    }
+  }
   // change code above this line
 }
 
 let bst = new BinarySearchTree();
+bst.add(8);
+bst.add(3);
+bst.add(3);
+bst.add(12);
+bst.add(12);
+// bst.add(15);
+// bst.add(1);
+// bst.add(1);
+
+var displayTree = (tree) => console.log(JSON.stringify(tree, null, 2));
+
+displayTree(bst.root);
