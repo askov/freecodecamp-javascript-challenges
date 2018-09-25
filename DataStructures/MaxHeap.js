deb3834d9f39fca0d481f2667698233a77539da0
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-09-25 23:13:23 +0700

    MaxHeap class: remove

--- DataStructures/MaxHeap.js
+++ DataStructures/MaxHeap.js
@@ -21,6 +21,38 @@ var MaxHeap = function() {
   this.print = function() {
     return this.nodes;
   };
+
+  this.remove = function() {
+    const sw = (nodes, index) => {
+      const lci = 2 * index + 1,
+        rci = 2 * (index + 1);
+      if (lci > nodes.length - 1) {
+        return;
+      }
+      const lc = nodes[lci],
+        rc = nodes[rci];
+      let swi;
+      if (lc && !rc) {
+        swi = lci;
+      } else if (rc && !lc) {
+        swi = rci;
+      } else {
+        swi = lc > rc ? lci : rci;
+      }
+      if (nodes[swi] > nodes[index]) {
+        const tmp = nodes[swi];
+        nodes[swi] = nodes[index];
+        nodes[index] = tmp;
+      }
+      sw(nodes, swi);
+    };
+
+    const root = this.nodes.shift(),
+      last = this.nodes.pop();
+    this.nodes.unshift(last);
+    sw(this.nodes, 0);
+    return root;
+  }
   // change code above this line
 };
 
@@ -28,26 +60,28 @@ var MaxHeap = function() {
 var mh = new MaxHeap();
 
 mh.insert(18);
-console.log(mh.print());
+// console.log(mh.print());
 
 mh.insert(19);
-console.log(mh.print());
+// console.log(mh.print());
 
 mh.insert(20);
-console.log(mh.print());
+// console.log(mh.print());
 
 
 mh.insert(10);
-console.log(mh.print());
+// console.log(mh.print());
 
 mh.insert(17);
-console.log(mh.print());
+// console.log(mh.print());
 
 mh.insert(11);
-console.log(mh.print());
+// console.log(mh.print());
 
 // mh.insert(11);
 mh.insert(21);
+
+mh.remove();
 console.log(mh.print());
 
 // console.log(mh.print());
